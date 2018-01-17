e7324a3e175f4e016346c0fc3f7eb5f41145280b
Julio-PC\Julio <juliobuonfigli@yahoo.com.ar>
2018-01-17 00:01:17 -0300

    Sin comentarios

--- src/main/java/ar/com/yahoojuliobuonfigli/imagej/MRF_main.java
+++ src/main/java/ar/com/yahoojuliobuonfigli/imagej/MRF_main.java
@@ -11,7 +11,8 @@ import ij.plugin.frame.*;
 //estructura de esta clase: run, main, dialogos, getters, instancia objeto ruler, instancia resultados
 //la clase ruler va a tomar todos los datos introducidos en los cuadros de diálogo y va a controlar el diagrama de flujo 
 //puede que sea una clase estática 
-//Rama: primeraRama
+//Rama: sinDispersion
+//Estoy pensano que ruler la voy a borrar a la chucha y todo lo que estaba ahi lo voy a meter en el metodo run
 
 public class MRF_main implements PlugIn {
 
@@ -25,31 +26,26 @@ public class MRF_main implements PlugIn {
 	private ImagePlus imp;
 	private String[] level = {"0,05", "0,01"};
 	private String[] M_operator = {"AND", "OR"};
-	private String[] D_function = {"Linear", "Quadratic", "Geometric", "Aritmethic"};
+//	private String[] D_function = {"Linear", "Quadratic", "Geometric", "Aritmethic"};
 	private String[] M_thresholds = {"No_threshold", "User_thresholds", "Default", "Huang", "Intermodes", "IsoData", "IJ_IsoData", "Li", 
 			"MaxEntropy", "Mean", "MinError", "Minimum", "Moments", "Otsu", "Percentile", "RenyiEntropy", "Shanbhag", "Triangle", "Yen"};
     private String[] S_thresholds = {"No_threshold", "User_thresholds", "MRF_threshold", "Default", "Huang", "Intermodes", "IsoData", 
     		"IJ_IsoData", "Li", "MaxEntropy", "Mean", "MinError", "Minimum", "Moments", "Otsu", "Percentile", "RenyiEntropy", "Shanbhag", "Triangle", "Yen"};
-	private RulerMRF getToWorkComputer;
-    static double RedMaskValue=0, GreenMaskValue=0, BlueMaskValue=0, RedSignalValue=0, GreenSignalValue=0, BlueSignalValue=0;
+  
+    static double RedMaskValue, GreenMaskValue, BlueMaskValue, RedSignalValue, GreenSignalValue, BlueSignalValue;
     
-   
     public boolean mainDialog() {
 	    GenericDialog gd = new GenericDialog("Multiple Relationship Finder");
-	    gd.addMessage("CHANNELS:");
+	   
 		gd.addChoice("Channel_1 (red): ", titles, titles[0]);
 	    gd.addChoice("Channel_2 (green): ", titles, titles[1]);
 	    gd.addChoice("Channel_3 (blue): ", titles2, titles2[2]);
-	    gd.addMessage("MASK OPTIONS:");
 	    gd.addChoice("Primary mask: ", titles2, titles2[2]);
 	    gd.addChoice("Masks thresholds: ",  M_thresholds, M_thresholds[0]);
 	    gd.addChoice("Red/Green operator", M_operator, M_operator[1]);
 	    gd.addChoice("ResultOfRG/Blue operator",  M_operator, M_operator[1]);
 	    gd.addCheckbox("Use channel specific masks", false);
-	    gd.addMessage("SIGNAL OPTIONS:");
 	    gd.addChoice("Signal thresholds: ", S_thresholds, S_thresholds[2]);
-	    gd.addCheckbox("Disperse signal", false);
-	    gd.addNumericField("Dispertion ratio: ", 4, 2);
 	    gd.addCheckbox("MORE OPTIONS", false);
 	    gd.showDialog();
 	    i1name = gd.getNextChoice();
@@ -61,8 +57,6 @@ public class MRF_main implements PlugIn {
         SelectedMO2 = gd.getNextChoice();
         Smasks = gd.getNextBoolean();
         SelectedST = gd.getNextChoice();
-        Dsignal = gd.getNextBoolean();
-        Dratio = gd.getNextNumber();
         Moptions = gd.getNextBoolean();
         
         i1 = WindowManager.getImage(i1name);
@@ -100,19 +94,16 @@ public class MRF_main implements PlugIn {
     public boolean moreOptionsDialog() {
       	 GenericDialog gd4 = new GenericDialog("More Options");
       	 
-      	gd4.addChoice("Dispertion function: ", D_function, D_function[2]);
- 	    gd4.addNumericField("MRF effective signal percentage: ", 50, 2);
+        gd4.addNumericField("MRF effective signal percentage: ", 50, 2);
  	    gd4.addCheckbox("Equalize intensities", false); //iguala los otros dos a la intensidad del menos intenso
- 	    gd4.addCheckbox("Erode mask when dispersing", true);
-		gd4.addChoice("Significance level: ", level, level[0]);
+ 	   	gd4.addChoice("Significance level: ", level, level[0]);
 	    gd4.addNumericField("Number of random coefficients: ", 30, 2);
  	    gd4.addCheckbox("Show examples of generated images", false);
  	    gd4.addNumericField("Random seed: ", 1, 3);
  	    gd4.showDialog();
-      	SelectedDF = gd4.getNextChoice();
+      	
  	    Tpercentage = gd4.getNextNumber();
         Aintensity = gd4.getNextBoolean();
-        Erode = gd4.getNextBoolean();
         SelectedSL = gd4.getNextChoice();
         GeneratedI = gd4.getNextNumber();
         Rimages = gd4.getNextBoolean();
@@ -132,8 +123,8 @@ public class MRF_main implements PlugIn {
 	static String getMaskOperator2 () { return SelectedMO2; }
 	static boolean getSpecificMask () { return Smasks; } 
 	static String getSignalThreshold () { return SelectedST; }
-	static boolean getDisperseSignal () { return Dsignal; }
-	static double getDisperseRatio () { return Dratio; }
+//	static boolean getDisperseSignal () { return Dsignal; }
+//	static double getDisperseRatio () { return Dratio; }
 	//static boolean getMoreOptions () { return Moptions; }
 	//gd2
 	static double getRedMaskValue () { return RedMaskValue; }
@@ -144,10 +135,10 @@ public class MRF_main implements PlugIn {
 	static double getGreenSignalValue () { return GreenSignalValue; }
 	static double getBlueSignalValue () { return BlueSignalValue; }
 	//gd4	
-	static String getDispersionFunction () { return SelectedDF; }
+//	static String getDispersionFunction () { return SelectedDF; }
 	static double getMFRthresholdPercentage () { return Tpercentage; } 
 	static boolean getAveregaIntensity () { return  Aintensity; } 
-	static boolean getErode () { return  Erode; } 
+//	static boolean getErode () { return  Erode; } 
 	static String getSignificanceLevel () { return SelectedSL; }
 	static double getGeneratedImages () { return GeneratedI; }
 	static boolean getShowRandomImages () { return Rimages; }
@@ -187,11 +178,9 @@ public class MRF_main implements PlugIn {
 	    	maskThresholdsDialog();
 	   if(SelectedST.equals("User_thresholds"))
 	    	signalThresholdsDialog();
-	  
-	   SelectedDF = "Aritmethic";
+  
 	   Tpercentage = 50.00;
        Aintensity = false;
-       Erode = true;
        SelectedSL = "0.05";
        GeneratedI = 30;
        Rimages = false;
@@ -204,12 +193,9 @@ public class MRF_main implements PlugIn {
 			   Dsignal, Tpercentage, Dratio, 12);
 	   ImagePlus i5=so.disperse();
 	   i5.show(); */
-	   
-	//getToWorkComputer = new RulerMRF();
-//	getToWorkComputer.runMRF();
-	}
-	
-	
+
+	}   
+  // supongo que cuando termine voy a tener que borrar de aca para abajo
 	public static void main(String[] args) {
 		// set the plugins.dir property to make the plugin appear in the Plugins menu
 		Class<?> clazz = MRF_main.class;
--- src/main/java/ar/com/yahoojuliobuonfigli/imagej/Mask.java
+++ src/main/java/ar/com/yahoojuliobuonfigli/imagej/Mask.java
@@ -1,6 +1,9 @@
 package ar.com.yahoojuliobuonfigli.imagej;
 
-//la clase vectorización tendra como un parametro de entrada un vector boolean que es la mascara
+//la clase vectorizacion tendra como un parametro de entrada un vector boolean que es la mascara
+//la mascara va a entregar tres mascaras para cada canal, cuando mascarasIndividuales 
+//sea falso las tres mascaras seran iguales 
+//vamos a hacer una mascara unica no convo no dispersion, no van stensel 
 
 import ij.plugin.PlugIn;
 import ij.*;
@@ -12,124 +15,137 @@ import ij.plugin.frame.*;
 
 public class Mask {
 
-	private String MaskThreshold, MaskOperator;
+	private String MaskThreshold, MaskOperator1, MaskOperator2;
 	private double redValue, greenValue, blueValue;
 	private ImagePlus red, green, blue, primaryMask;
-	private double [] Red, Green, Blue, Mask, ThresholdsMask;
-	private int h, w, s=4;
-
+	private boolean [] Red, Green, Blue, Mask, ThresholdsMask;
+	private int h, w;
+   // private boolean operate;
+    
 	public Mask(ImagePlus primaryMask, ImagePlus red, ImagePlus green, ImagePlus blue, String MaskThreshold, 
-			String MaskOperator, double redValue, double greenValue, double blueValue ) {
+	            String MaskOperator1, String MaskOperator2, double redValue, double greenValue, 
+	            double blueValue)
+		{
 		this.red=red;
 		this.green=green;
 		this.blue=blue;
 		this.primaryMask=primaryMask;
 		this.MaskThreshold=MaskThreshold;
-		this.MaskOperator=MaskOperator;
+		this.MaskOperator1=MaskOperator1;
+		this.MaskOperator2=MaskOperator2;
 		this.redValue=redValue;
 		this.greenValue=greenValue;
 		this.blueValue=blueValue;
 		IJ.run(red, "8-bit", ""); 
 		IJ.run(green, "8-bit", "");
-		IJ.run(blue, "8-bit", "");	
-		if(red.equals(null))
-		{h=green.getHeight(); w=green.getWidth(); }
-		else
-		{h=red.getHeight(); w=red.getWidth(); }
-		Red = new double[h*w];
-		Green = new double[h*w];
-		Blue = new double[h*w];
-		Mask = new double[h*w];
-		ThresholdsMask = new double[h*w];
-	}
+		if(blue.equals(null)) {} else
+		{IJ.run(blue, "8-bit", "");}	
+		h=red.getHeight(); 
+		w=red.getWidth(); 
+		Red = new boolean[h*w];
+		Green = new boolean[h*w];
+		Blue = new boolean[h*w];
+		Mask = new boolean[h*w];
+		ThresholdsMask = new boolean[h*w]; 
+	
+		}
 
-	public double[] binarizeAndVectorizeChannel(ImagePlus Channel, double value) {
-		double[] vector = new double[w*h];
-		ImageProcessor CHANNEL;
+	    public boolean[] binarizeChannel(ImagePlus Channel, double value) 
+	    {
+		boolean[] VECTOR = new boolean[w*h];
 		if(MaskThreshold.equals("User_threshold")) {
 			IJ.setThreshold(Channel, value, 255);
 			IJ.run(Channel, "Convert to Mask", ""); }
 		else {		
 			IJ.setAutoThreshold(Channel, MaskThreshold);
 			IJ.run(Channel, "Convert to Mask", ""); }
-		CHANNEL=Channel.getProcessor(); 
+		ImageProcessor CHANNEL=Channel.getProcessor(); 
 		int i=0;
 		for(int y=0; y<h; y++)
-		{
+			{
 			for(int x=0; x<w; x++)
-			{
-				vector[i]=CHANNEL.getPixel(x, y);
+				{
+				if(CHANNEL.getPixel(x, y)==255)
+					VECTOR[i]=true;
+				else
+					VECTOR[i]=false;
 				i++;
-			}
-		}
-		return vector;
-	}
-
-	public boolean[] createMask() {
-		
-		double[] finalMask = new double[w*h];
-		boolean[] booleanFinalMask = new boolean[w*h];
-
-		if(MaskThreshold.equals("No_thresold")) 
-		{ for(int i=0; i<w*h; i++) ThresholdsMask[i] = 255; } 
-		else {
-			if(red.equals(null)) {s=1;} else { Red=binarizeAndVectorizeChannel(red, redValue); }
-			if(green.equals(null)) {s=2;} else { Green=binarizeAndVectorizeChannel(green, greenValue); }
-			if(blue.equals(null)) {s=3;} else { Blue=binarizeAndVectorizeChannel(blue, blueValue); }
-
-			if(MaskOperator.equals("AND"))
-			{
-				switch(s) {
-				case 1: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Green[i]*Blue[i];
-					break;
-				case 2: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Red[i]*Blue[i];
-					break;
-				case 3: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Green[i]*Red[i];
-					break;
-				case 4: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Red[i]*Green[i]*Blue[i];
-					break;
 				}
 			}
-			else
-			{
-				switch(s) {
-				case 1: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Green[i]+Blue[i];
-				case 2: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Red[i]+Blue[i];
-				case 3: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Green[i]+Red[i];
-				case 4: 
-					for(int i=0; i<w*h; i++)
-						ThresholdsMask[i] = Red[i]+Green[i]+Blue[i];			}
-			}
+		return VECTOR;
 		}
-		if(primaryMask.equals(null)) { finalMask = ThresholdsMask; } 	else {
-			Mask=binarizeAndVectorizeChannel(primaryMask, 200); 
-			for(int i=0; i<w*h; i++)
-				finalMask[i] = ThresholdsMask[i]*Mask[i]; 
 
-		for(int i=0; i<w*h; i++)
-			if(finalMask[i]>0)
-				booleanFinalMask[i] = true;
+	public boolean[] createMask() 
+	{
+		
+		boolean[] finalMask = new boolean[w*h];
+		
+		if(MaskThreshold.equals("No_thresold")) 
+			 { for(int i=0; i<w*h; i++) ThresholdsMask[i] = true; } 
+			 else 
+			 {
+			 Red=binarizeChannel(red, redValue); 
+			 Green=binarizeChannel(green, greenValue); 
+			if(blue.equals(null)) {} else { Blue=binarizeChannel(blue, blueValue); }
+     		if(MaskOperator1.equals("AND"))
+				{
+				for(int i=0; i<w*h; i++)
+					{
+					if(Red[i]==true && Green[i]==true)
+						ThresholdsMask[i] = true;
+					else
+						ThresholdsMask[i] = false;
+					}
+				}
 			else
-				booleanFinalMask[i] = false;
-				
+				{
+				for(int i=0; i<w*h; i++)
+					{
+					if(Red[i]==true || Green[i]==true)
+						ThresholdsMask[i] = true;
+					else
+						ThresholdsMask[i] = false;
+					}
+				}
+     		if(blue.equals(null)) {} else
+     			{
+     			if(MaskOperator2.equals("AND"))
+     				{
+     				for(int i=0; i<w*h; i++)
+     					{
+     					if(ThresholdsMask[i]==true && Blue[i]==true)
+     						ThresholdsMask[i] = true;
+     					else
+     						ThresholdsMask[i] = false;
+     					}
+     				}
+     			else
+     				{
+     				for(int i=0; i<w*h; i++)
+     					{
+     					if(ThresholdsMask[i]==true || Blue[i]==true)
+     						ThresholdsMask[i] = true;
+     					else
+     						ThresholdsMask[i] = false;
+     					}
+     				}
+     			}
+			}
+     		if(primaryMask.equals(null)) { finalMask = ThresholdsMask; } 	else {
+			Mask=binarizeChannel(primaryMask, 200); 
+			for(int i=0; i<w*h; i++)
+				{
+				if(ThresholdsMask[i]==true && Mask[i]==true)
+					finalMask[i] = true;
+				else
+					finalMask[i] = false;
+				}
+
+		return finalMask;
+			}
+			return finalMask;
 		}
-		return booleanFinalMask;
 	}
-}
 
 
 
